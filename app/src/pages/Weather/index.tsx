0ea13e7485f31c4f32322c9ec9d115d59d94b991
Jaakko Humalajoki <jaakko.humalajoki@tuni.fi>
2021-03-09 15:25:22 +0200

    Add Highcharts to Weather page
    Uses services/fmi to request weather data for Tampere region and
    implements a basic highcharts graph on weather page
    
    Resolves: #21

--- app/src/pages/Weather/index.tsx
+++ app/src/pages/Weather/index.tsx
@@ -1,5 +1,24 @@
-import * as React from "react"
+import React, { useState, useEffect } from "react"
+import ComparisonChart from "./ComparisonChart"
+import weatherService, {
+  WeatherData,
+  WeatherDataPoint,
+} from "../../services/fmi"
 
 export default (): JSX.Element => {
-  return <div>Weather</div>
+  const [data, setData] = useState<WeatherDataPoint[]>([])
+  useEffect(() => {
+    weatherService
+      .getWeatherData("Tampere")
+      .then((weatherData: WeatherData) => {
+        setData(weatherData.data)
+      })
+  }, [])
+
+  return (
+    <div>
+      <h1>Electricity</h1>
+      <ComparisonChart weatherData={data} />
+    </div>
+  )
 }
