8a7772a1b6faf105b18c7fb1caa6836071359e59
Jaakko Humalajoki <jaakko.humalajoki@tuni.fi>
2021-03-12 17:16:35 +0200

    Add Highcharts to electricity page
    The Electricity page makes 4 separate requests to Fingrid service to
    pull consumption, nuclear, hydro and wind electricity data from the past
    10 days to display on a single Highcharts comparison graph.
    
    Resolves: #14

---
+++ app/src/pages/Electricity/ComparisonChart.tsx
@@ -0,0 +1,121 @@
+import React from "react"
+import Highcharts from "highcharts/highstock"
+import HighchartsReact from "highcharts-react-official"
+import { ElectricityDataPoint } from "../../services/fingrid"
+
+interface Props {
+  consumptionData: ElectricityDataPoint[]
+  nuclearData: ElectricityDataPoint[]
+  hydroData: ElectricityDataPoint[]
+  windData: ElectricityDataPoint[]
+}
+
+export default (props: Props): JSX.Element => {
+  const { consumptionData, nuclearData, hydroData, windData } = props
+
+  const options: Highcharts.Options = {
+    title: {
+      text: "Energy comparison",
+    },
+    navigator: {
+      enabled: true,
+      maskFill: "rgba(0, 82, 156, 0.3)",
+      series: { color: "rgba(0, 82, 156, 0.3)" },
+      adaptToUpdatedData: true,
+    },
+    rangeSelector: {
+      enabled: true,
+      verticalAlign: "bottom",
+      buttons: [
+        {
+          type: "day",
+          count: 1,
+          text: "1d",
+          title: "View a day",
+        },
+        {
+          type: "day",
+          count: 7,
+          text: "1w",
+          title: "View a week",
+        },
+        {
+          type: "month",
+          count: 1,
+          text: "1m",
+          title: "View a month",
+        },
+        {
+          type: "all",
+          text: "All",
+          title: "View all",
+        },
+      ],
+    },
+    xAxis: {
+      type: "datetime",
+      crosshair: true,
+    },
+    yAxis: [
+      {
+        title: {
+          text: "Electricity MW",
+        },
+        lineWidth: 2,
+        resize: {
+          enabled: true,
+        },
+      },
+    ],
+    tooltip: {
+      shared: true,
+    },
+    series: [
+      {
+        type: "line",
+        name: "Total consumption",
+        showInNavigator: true,
+        data: consumptionData.map((point) => [
+          point.time.getTime(),
+          point.value,
+        ]),
+        tooltip: {
+          valueSuffix: " MW",
+        },
+      },
+      {
+        type: "line",
+        name: "Nuclear Energy",
+        showInNavigator: true,
+        data: nuclearData.map((point) => [point.time.getTime(), point.value]),
+        tooltip: {
+          valueSuffix: " MW",
+        },
+      },
+      {
+        type: "line",
+        name: "Hydro Energy",
+        showInNavigator: true,
+        data: hydroData.map((point) => [point.time.getTime(), point.value]),
+        tooltip: {
+          valueSuffix: " MW",
+        },
+      },
+      {
+        type: "line",
+        name: "Wind Energy",
+        showInNavigator: true,
+        data: windData.map((point) => [point.time.getTime(), point.value]),
+        tooltip: {
+          valueSuffix: " MW",
+        },
+      },
+    ],
+  }
+
+  return (
+    <div>
+      <HighchartsReact highcharts={Highcharts} options={options} />
+    </div>
+  )
+}
--- app/src/pages/Electricity/index.tsx
+++ app/src/pages/Electricity/index.tsx
@@ -1,5 +1,67 @@
-import * as React from "react"
+import React, { useEffect, useState } from "react"
+import ComparisonChart from "./ComparisonChart"
+import fingridService, { ElectricityDataPoint } from "../../services/fingrid"
+import { variables } from "../../services/fingrid-types"
 
 export default (): JSX.Element => {
-  return <div>Electricity</div>
+  const [nuclearData, setNuclearData] = useState<ElectricityDataPoint[]>([])
+  const [hydroData, setHydroData] = useState<ElectricityDataPoint[]>([])
+  const [windData, setWindData] = useState<ElectricityDataPoint[]>([])
+  const [consumptionData, setConsumptionData] = useState<
+    ElectricityDataPoint[]
+  >([])
+  // const [forecastWindData, setForecastWindData] = useState<
+  //   ElectricityDataPoint[]
+  // >([])
+
+  const today: Date = new Date()
+  const tenDaysPast: Date = new Date()
+  tenDaysPast.setDate(today.getDate() - 10)
+
+  useEffect(() => {
+    fingridService
+      .getElectricityData(variables.consumptionForecast, tenDaysPast, today)
+      .then((electricityData) => {
+        setConsumptionData(electricityData.data)
+      })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  useEffect(() => {
+    fingridService
+      .getElectricityData(variables.productionNuclear, tenDaysPast, today)
+      .then((electricityData) => {
+        setNuclearData(electricityData.data)
+      })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  useEffect(() => {
+    fingridService
+      .getElectricityData(variables.productionHydro, tenDaysPast, today)
+      .then((electricityData) => {
+        setHydroData(electricityData.data)
+      })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  useEffect(() => {
+    fingridService
+      .getElectricityData(variables.productionWind, tenDaysPast, today)
+      .then((electricityData) => {
+        setWindData(electricityData.data)
+      })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  return (
+    <div>
+      <ComparisonChart
+        consumptionData={consumptionData}
+        nuclearData={nuclearData}
+        hydroData={hydroData}
+        windData={windData}
+      />
+    </div>
+  )
 }
