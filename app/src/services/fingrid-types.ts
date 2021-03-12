80c6bd621ff128cd5f1355e18055a158ca7f4268
Jaakko Humalajoki <jaakko.humalajoki@tuni.fi>
2021-03-12 18:03:09 +0200

    Add new fingrid endpoint variables

--- app/src/services/fingrid-types.ts
+++ app/src/services/fingrid-types.ts
@@ -1,10 +1,13 @@
 export const variables = Object.freeze({
   // Forecasts
   consumptionForecast: 166,
+  productionForecastTotal: 242,
   productionForecastWind: 245,
   productionForecastSolar: 248,
 
   // Real-time data
+  consumptionTotal: 124,
+  productionTotal: 74,
   productionNuclear: 188,
   productionHydro: 191,
   productionWind: 181,
