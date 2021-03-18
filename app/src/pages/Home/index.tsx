c17061c7df396178e9fbe1befb563e6daa464da2
Toni Immonen <toni.immonen@tuni.fi>
2021-03-19 00:07:23 +0200

    Create simple landing page
    The page contains a heading and two buttons to navigate to Weather and Electricity pages

--- app/src/pages/Home/index.tsx
+++ app/src/pages/Home/index.tsx
@@ -1,5 +1,30 @@
-import * as React from "react"
+import React from "react"
+import { Link } from "react-router-dom"
 
 export default (): JSX.Element => {
-  return <div>Home</div>
+  return (
+    <div className="mx-auto my-32 max-w-lg space-y-8 text-center flex flex-col">
+      <h3 className="m-2 text-4xl font-bold">
+        Real-time data about weather and electricity
+      </h3>
+      <div className="bg-white bg-opacity-60 p-2 m-4 rounded-lg max-w-xl self-center">
+        <Link to="weather">
+          <button
+            type="button"
+            className="btn-rounded-lg text-gray-500 bg-white hover:bg-gray-700 hover:text-white"
+          >
+            Weather
+          </button>
+        </Link>
+        <Link to="electricity">
+          <button
+            type="button"
+            className="btn-rounded-lg text-white bg-gray-500 hover:bg-gray-700"
+          >
+            Electricity
+          </button>
+        </Link>
+      </div>
+    </div>
+  )
 }
