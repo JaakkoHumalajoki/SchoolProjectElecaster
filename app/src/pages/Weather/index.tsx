a660387dec13655d50834509ea8a86362b67e885
Jaakko Humalajoki <jaakko.humalajoki@tuni.fi>
2021-03-18 21:17:46 +0200

    Add city selection to Weather page

--- app/src/pages/Weather/index.tsx
+++ app/src/pages/Weather/index.tsx
@@ -7,17 +7,26 @@ import weatherService, {
 
 export default (): JSX.Element => {
   const [data, setData] = useState<WeatherDataPoint[]>([])
+  const [city, setCity] = useState<string>("Tampere")
+
   useEffect(() => {
-    weatherService
-      .getWeatherData("Tampere")
-      .then((weatherData: WeatherData) => {
-        setData(weatherData.data)
-      })
-  }, [])
+    weatherService.getWeatherData(city).then((weatherData: WeatherData) => {
+      setData(weatherData.data)
+    })
+  }, [city])
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const handleCityChange = (event: any) => {
+    setCity(event.target.value)
+  }
 
   return (
     <div>
-      <h1>Weather</h1>
+      <select name="city" onChange={handleCityChange}>
+        <option value="tampere">Tampere</option>
+        <option value="helsinki">Helsinki</option>
+        <option value="jyväskylä">Jyväskylä</option>
+      </select>
       <ComparisonChart weatherData={data} />
     </div>
   )
