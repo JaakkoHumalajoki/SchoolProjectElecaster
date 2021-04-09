730a9b36216790c362dfd3f3a6960dea6a47c416
siikanen <santeri.siiranen@pm.me>
2021-04-09 11:04:09 +0300

    Add docstrings to WeatherService

--- app/src/services/fmi.ts
+++ app/src/services/fmi.ts
@@ -10,12 +10,24 @@ import { FmiForecastResponse } from "./fmi-types"
  * @property timerange {TimeRange}
  */
 class WeatherService implements WeatherData {
+  /**
+   * City that the service currently represents
+   */
   public city: City
 
+  /**
+   * Timerange that the service currently represents
+   */
   private historyTimeRange: TimeRange
 
+  /**
+   * Internal container for storing the weatherdata
+   */
   private weatherData: WeatherData
 
+  /**
+   * Parser to be used for paring XML responses from FMI
+   */
   private readonly domParser = new DOMParser()
 
   // TODO: just a placeholder whilst all depending components
@@ -47,20 +59,36 @@ class WeatherService implements WeatherData {
     this.historyTimeRange = timeRange
   }
 
+  /**
+   * Getter for history data. Exposes class data read-only
+   */
   get history(): WeatherData["history"] {
     return this.weatherData.history
   }
 
+  /**
+   * Getter for forecast data. Exposes class data read-only
+   */
   get forecast(): WeatherData["forecast"] {
     return this.weatherData.forecast
   }
 
+  /**
+   * Convert Date to FMI compatible string
+   */
   private dateToFmiString = (date: Date): string =>
     date.toISOString().replace(/.\d+Z$/g, "Z")
 
+  /**
+   * Return forecast URL for current City
+   */
   private getForecastUrl = (city: City): string =>
     `https://www.ilmatieteenlaitos.fi/api/weather/forecasts?area=&place=${city.name.toLowerCase()}`
 
+  /**
+   * Return history URL for current city and timerange
+   * @param timeRange timerange to bo set for the URL
+   */
   private getHistoryUrl(timeRange: TimeRange): string {
     return (
       `https://opendata.fmi.fi/wfs?request=getFeature&version=2.0.0&storedquery_id=fmi::observations::weather::multipointcoverage&parameters=t2m,wd_10min,ws_10min,r_1h&timestep=60` +
@@ -73,6 +101,7 @@ class WeatherService implements WeatherData {
   }
 
   /**
+   * Set the timerange for the class to represent
    * @param timerange {TimeRange} timerange to fetch data for
    */
   public set timeRange(timeRange: TimeRange) {
@@ -82,10 +111,18 @@ class WeatherService implements WeatherData {
     this.historyTimeRange = timeRange
   }
 
+  /**
+   * Timerange that the service currently represents
+   */
   public get timeRange(): TimeRange {
     return this.historyTimeRange
   }
 
+  /**
+   * Fetches all fields from Fingrid api for the current timerange
+   *
+   * This method MUST be called before accessing the data!
+   */
   public async fetch(): Promise<void> {
     await Promise.allSettled([
       this.updateForecastData(),
@@ -93,6 +130,9 @@ class WeatherService implements WeatherData {
     ])
   }
 
+  /**
+   * Refreshes historydata
+   */
   private async updateHistoryData(): Promise<void> {
     const historyUrls = await this.generateHistoryUrls()
 
@@ -108,6 +148,9 @@ class WeatherService implements WeatherData {
     })
   }
 
+  /** Generates list of history URLs to be fetched for the current
+   * timerange
+   */
   private async generateHistoryUrls(): Promise<string[]> {
     const timeHolder: TimeRange = {
       startTime: new Date(this.historyTimeRange.startTime),
@@ -134,6 +177,9 @@ class WeatherService implements WeatherData {
     return historyUrls
   }
 
+  /**
+   * Refreshes forecastdata
+   */
   private async updateForecastData(): Promise<void> {
     const response: AxiosResponse<FmiForecastResponse> = await axios.get(
       this.getForecastUrl(this.city)
@@ -170,6 +216,11 @@ class WeatherService implements WeatherData {
       )
   }
 
+  /**
+   * Parse XML response from fmi multipointcoverage
+   * @param rawXml xml document from FMI
+   * @returns WeatherDataPoint[] parsed weatherdatapoints
+   */
   private parseXMLResponse(rawXml: string): WeatherDataPoint[] {
     const geoXml: Document = this.domParser.parseFromString(rawXml)
 
