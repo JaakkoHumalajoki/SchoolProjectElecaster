0ab4fbbb6547d15eb19157487b84196842bbeed3
Jaakko Humalajoki <jaakko.humalajoki@tuni.fi>
2021-04-14 14:35:49 +0300

    Fix typo in history analysis chart

--- app/src/pages/Analysis/HistoryChart.tsx
+++ app/src/pages/Analysis/HistoryChart.tsx
@@ -50,7 +50,7 @@ const HistoryChart = (props: Props): JSX.Element => {
 
   const options: Highcharts.Options = {
     title: {
-      text: "Energy & Weather forecast analysis",
+      text: "Energy & Weather history analysis",
     },
     chart: {
       height: "600px",
