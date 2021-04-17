a81c5e15fcec9b80337042b08d64b8695d85666c
siikanen <santeri.siiranen@gmail.com>
2021-04-17 16:51:46 +0300

    Change fingrid  datavariables to strings
    They are part of the url so its useless conversion

--- app/src/services/fingrid-types.ts
+++ app/src/services/fingrid-types.ts
@@ -1,16 +1,16 @@
 export const fingridVariables = Object.freeze({
   // Forecasts
-  consumptionForecast: 166,
-  productionForecastTotal: 242,
-  productionForecastWind: 245,
-  productionForecastSolar: 248,
+  consumptionForecast: "166",
+  productionForecastTotal: "242",
+  productionForecastWind: "245",
+  productionForecastSolar: "248",
 
   // Real-time data
-  consumptionTotal: 124,
-  productionTotal: 74,
-  productionNuclear: 188,
-  productionHydro: 191,
-  productionWind: 181,
+  consumptionTotal: "124",
+  productionTotal: "74",
+  productionNuclear: "188",
+  productionHydro: "191",
+  productionWind: "181",
 })
 
 export interface FingridDatapoint {
