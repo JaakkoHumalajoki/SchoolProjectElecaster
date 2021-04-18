4092bd43d6340bc5d1c8dda1353942c449ad44ee
Toni Immonen <toni.immonen@tuni.fi>
2021-04-18 21:58:48 +0300

    Add thickness to header border

--- app/src/layout/Header/Header.tsx
+++ app/src/layout/Header/Header.tsx
@@ -12,7 +12,7 @@ export default (): JSX.Element => {
   }
 
   return (
-    <header className="border mb-3 bg-white bg-opacity-70">
+    <header className="border-2 mb-3 bg-white bg-opacity-70">
       <div className="navbar">
         <div className="flex flex-auto">
           <Link to="/" onClick={closeNavMenu}>
