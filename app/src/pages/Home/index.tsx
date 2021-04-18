6413ce3180c057e189faa45a77e0ebcf9494916a
Toni Immonen <toni.immonen@tuni.fi>
2021-04-18 14:50:13 +0300

    Apply flex layout around home page's buttons

--- app/src/pages/Home/index.tsx
+++ app/src/pages/Home/index.tsx
@@ -7,7 +7,7 @@ export default (): JSX.Element => {
       <h3 className="m-2 text-4xl font-bold">
         Real-time data about weather and electricity
       </h3>
-      <div className="bg-white bg-opacity-60 p-2 m-4 rounded-lg max-w-xl self-center">
+      <div className="flex flex-col xs:flex-row self-center space-y-4 xs:space-y-0 xs:space-x-6">
         <Link to="weather">
           <button
             type="button"
