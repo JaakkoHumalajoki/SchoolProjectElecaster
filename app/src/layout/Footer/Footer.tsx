3b1b766afacad3a0a0fd31640d30aad5fccd3ae8
siikanen <santeri.siiranen@gmail.com>
2021-04-19 21:58:25 +0300

    Add explanation about the application to footer

--- app/src/layout/Footer/Footer.tsx
+++ app/src/layout/Footer/Footer.tsx
@@ -1,9 +1,14 @@
 import React from "react"
 
 export default (): JSX.Element => (
-  <footer className="bg-gray-300 h-48 shadow-inner relative w-full -bottom-48">
+  <footer className="bg-gray-300 h-72 sm:h-48 shadow-inner relative w-full -bottom-72 sm:-bottom-48">
     <div className="max-w-2xl flex flex-col-reverse sm:flex-row justify-evenly items-center mt-0 m-4 mx-auto">
       <div className="">
+        <p className="m-4 pt-2 text-center sm:text-left">
+          Elecaster is an application for visualizing weather and electricity
+          data history and forecast in Finland. The data is fetched directly
+          from Finnish Meteorological Institute and Fingrid by your browser.
+        </p>
         <p className="m-4 text-center sm:text-left">
           Elecaster is Tampere University course COMP.SE.110-2020-2021-1
           Software Design project. It was built during spring 2021
